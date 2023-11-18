0311085f2cb38ef00fbe7d16176c14b6703c9831
amit om kashyap <amit.ece.21@nitap.ac.in>
2023-11-18 23:29:42 +0530

    Day4

--- logic/click_handling.js
+++ logic/click_handling.js
@@ -1,10 +1,16 @@
 const allsquare=document.getElementsByClassName("square");
+const clickeditem=[];
 for(const i of allsquare){
    i.addEventListener("click",()=>{
     const idofelement=i.getAttribute("id");
     const innerhtml=document.getElementById(idofelement).innerHTML;
+    if(clickeditem.length>0){
+        clickeditem[0].removeAttribute("style");
+        clickeditem.length=0;
+    }
     if(innerhtml.includes("black") || innerhtml.includes("white")){
         document.getElementById(idofelement).style.backgroundColor="yellow";
+        clickeditem.push(i)
     }
    })
 }
