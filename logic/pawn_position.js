140250d4a0bb42ea662f6854738b0dd1d88adca7
amit om kashyap <amit.ece.21@nitap.ac.in>
2024-06-22 22:20:44 +0530

    pawn steps circle

--- logic/pawn_position.js
+++ logic/pawn_position.js
@@ -4,7 +4,7 @@ for(const i of filesnamearray){ //filesname from mapping.js
     pownpositionblack.push(document.getElementById(i+2))
 }
 for(const j of filesnamearray){
-    pownpositionblack.push(document.getElementById(j+7))
+    pownpositionwhite.push(document.getElementById(j+7))
 }
 for(const k of pownpositionblack){
     k.addEventListener("click",()=>{
@@ -27,9 +27,24 @@ for(const k of pownpositionwhite){
         const steps=[];
         for(let k=0;k<2;k++){
             change--;
-            steps.push(document.getElementById(id[0]-change))
+            steps.push(document.getElementById(id[0]+change))
         }
            console.log(steps);
-           console.log(steps);
+           hightlightCircle(stepsSquare);
+         
     })
 }
+
+// function to add highlighting
+
+const hightlightCircle = function ([first, second]) {
+    let circle = document.createElement("div");
+    let circle2 = document.createElement("div");
+    circle.setAttribute("class", "circle");
+    circle2.setAttribute("class", "circle");
+    first.appendChild(circle);
+    second.appendChild(circle2);
+    console.log(first, second);
+    first.classList.add("flex");
+    second.classList.add("flex");
+  };
